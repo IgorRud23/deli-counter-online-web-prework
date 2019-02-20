90bffae968e0228a21422b7d640d072b1e20ae36
Igor Rud <rud.igor23@gmail.com>
2019-02-20 21:31:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,8 @@ katz_deli = []
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
-  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-  return name, position
+  puts "Welcome, #{name}. You are number #{position+1} in line."
 end
-
-take_a_number(katz_deli, "Logan")
-take_a_number(katz_deli, "Avi")
-take_a_number(katz_deli, "Spencer")
 
 def line(array)
 if array.length == 0
@@ -22,8 +17,6 @@ else
   puts "#{message}"
 end
 end
-
-
 
 def now_serving(array)
   if array.empty? == true
